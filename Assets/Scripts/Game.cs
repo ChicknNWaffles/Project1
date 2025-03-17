905428beda4407ae686e3b324c23a4a411d619a7
Austin Erhart <austinerhartm@gmail.com>
2025-03-17 18:43:19 -0500

    Added Start menu, pause menu, and game over menus
    Added a start menu using TextMeshPro Ui elements, Play button currently moves to scene 1 in the build order can be changed to whatever scene necessary based on build order, added a blank settings for maybe showing controls, quit button quits, pause menu can be paused with esc and unpaused, there is also a button to unpause, pausing is taking place of the timescale and changes between 1 (play) and 0 (paused), main menu button reverts scene back to scene 0 or the main menu.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,19 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour{
     public static ShooterControls Input { get; private set; }
+    public GameObject gameOverScreen;
+    public GameObject pauseMenu;
+    public SpriteRenderer player;
+    public Camera camera;
 
     // Start is called before the first frame update
     void Start(){
+
         Input = new ShooterControls();
         Input.Enable();
+        SceneManager.activeSceneChanged += disableInput;
 
     }
 
     // Update is called once per frame
     void Update(){
-        
+
+        if (Input.Standard.Pause.triggered) {
+            OnPause();
+        }
+
+        GameOver();
+
+    }
+
+    private void disableInput() {
+
+        Input.Standard.Disable();
+
+    }
+
+    private void disableInput(Scene curr, Scene next) {
+
+        Input.Standard.Disable();
+
+    }
+
+    private void enableInput() {
+
+        Input.Standard.Enable();
+
+    }
+
+    public void GameOver()
+    {
+
+        if (IsOffScreenVertically(player, camera))
+        {
+
+            gameOverScreen.SetActive(true);
+            disableInput();
+
+        }
+
+    }
+
+    private void OnPause() {
+
+        togglePause();
+
+    }
+
+    public bool togglePause() {
+        if (Time.timeScale == 0f) {
+
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            return (false);
+
+        }
+        else {
+
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            return (true);
+
+        }
+    }
+
+    private bool IsOffScreenVertically(SpriteRenderer sprite, Camera camera)
+    {
+
+        var bounds = sprite.bounds;
+        var top = camera.WorldToViewportPoint(bounds.max);
+        var bottom = camera.WorldToViewportPoint(bounds.min);
+        return top.y < 0 || bottom.y > 1;
+
     }
 }
