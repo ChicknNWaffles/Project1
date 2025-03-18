92e9848cfaf811ee20f0bd3e1a89a7e736ca2b35
ChicknNWaffles <elora.browning@gmail.com>
2025-03-18 02:33:22 -0500

    more progress toward enemy movement
    The enemy type that stays in the back and moves up and down is now moving right. I also have measures in place to stop this enemy type from going off screen. This could easily be adapted later to stop the player from going offscreen as well.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,22 +15,64 @@ public class Enemy : MonoBehaviour
     }
     public Type type;
     public float moveSpeed;
+    private Vector3 target;
+    public float turnLikelihood;
+    private float curTurnLikelihood;
+    private Vector3 direction;
 
     // Start is called before the first frame update
     void Start(){
-
+        var curTurnLikelihood = turnLikelihood;
+        var direction = Vector3.left;
+        InvokeRepeating("pathfind", 0f, 0.5f);
     }
 
     // Update is called once per frame
     void Update(){
-        // Movement
+        // Moves enemy towards target chosen by pathfinder
+        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+    }
+
+    // handles enemy pathfinding differently depending on enemy type
+    void pathfind(){
         if(type == Type.MovingFighter){
 
         }else if(type == Type.FormationFighterMoving || type == Type.Station || type == Type.Asteroid){
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-        }
-        else if (type == Type.BackFighter){
+            target = transform.position + (Vector3.left * moveSpeed);
+        }else if (type == Type.BackFighter){
+            // if this is the first time setting the target, pick
+            // a random direction. Else, decide whether or not
+            // to turn around.
+            if (direction == Vector3.left) {
+                if (Random.value >= 0.5f) {
+                    direction = Vector3.up;
+                } else {
+                    direction = Vector3.down;
+                }
+            }else { 
+                if(Random.value <= curTurnLikelihood) {
+                    if(direction == Vector3.up) {
+                        direction = Vector3.down;
+                    } else {
+                        direction = Vector3.up;
+                    }
+                }
+            }
+            target = transform.position + (direction * moveSpeed);
+        } 
+    }
 
+    // Detects when the enemy collides with another object
+    // or collider
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.transform.parent.gameObject.name.Equals("OffscreenHitbox")){
+            curTurnLikelihood = 1.0f;
+        }else if (collision.transform.parent.gameObject.name.Equals("TurnSoon-High")){
+            curTurnLikelihood = turnLikelihood + 7 * ((1.0f - turnLikelihood)/12);
+        }else if (collision.transform.parent.gameObject.name.Equals("TurnSoon-Low")){
+            curTurnLikelihood = turnLikelihood + 3 * ((1.0f - turnLikelihood)/12);
+        }else if (collision.transform.parent.gameObject.name.Equals("NormalTurn")){
+            curTurnLikelihood = turnLikelihood;
         }
     }
 }
