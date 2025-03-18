98c1ce445939ea68fe83faf41db23588ee90d398
ChicknNWaffles <elora.browning@gmail.com>
2025-03-18 03:06:07 -0500

    Player screen-edge collision
    The player now can't go offscreen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour{
     public float moveSpeed;
+    private float canGoUp = 1.0f;
+    private float canGoDown = 1.0f;
 
     // Start is called before the first frame update
     void Start(){
-        
+
     }
 
     // Update is called once per frame
     void Update(){
         var input = Game.Input.Standard;
-        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime * input.MoveUp.ReadValue<float>());
-        transform.Translate(Vector3.down * moveSpeed * Time.deltaTime * input.MoveDown.ReadValue<float>());
+        print("canGoUp is " + canGoUp + " and canGoDown is " + canGoDown);
+        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime * input.MoveUp.ReadValue<float>() * canGoUp);
+        transform.Translate(Vector3.down * moveSpeed * Time.deltaTime * input.MoveDown.ReadValue<float>() * canGoDown);
 
+    }
+
+    // detects if the player is going offscreen
+    // and stops them
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.transform.parent.gameObject.name.Equals("OffscreenHitbox")){
+            if(collision.name.Equals("Top")){
+                print("gong offscreen");
+                canGoUp = 0.0f;
+            } else {
+                print("gong offscreen");
+                canGoDown = 0.0f;
+            }
+        }
+    }
+
+    // detects when player is no longer
+    // about to go offscreen and re-enables
+    // movement in that direction
+    void OnTriggerExit2D(Collider2D collision) { 
+        if(collision.transform.parent.gameObject.name.Equals("OffscreenHitbox")){
+            canGoUp = 1.0f;
+            canGoDown = 1.0f;
+        }
     }
 }
